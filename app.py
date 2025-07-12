0344d0d24048da48e79aac9083d841e2a043c5ae
ahrisam <ahrisam3@outlook.com>
2025-07-12 14:57:30 +0000

    design

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
-import sys, csv, datetime, os
+import sys, csv, datetime
+from pathlib import Path
 import qtawesome as qta
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QDate
 
-data_path = "data/expenses.csv"
+data_path = Path("data")/"expense.csv"
 #creating csv file and file directory for saving the data
+def expense(row):
+    data_path.parent.mkdir(parents=True, exist_ok=True)
+    file_exists = data_path.exists()
+    with data_path.open("a", newline='') as f:
+        writer = csv.writer(f)
+        if not file_exists:
+            writer.writerow(["Date", "Amount","Category", "Description"])
+        writer.writerow(row)
+
+def loading_csv_table(table, path):
+    if not Path(data_path).exists():
+        return
+    with open(path, newline='') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+    if data:
+        headers = data[0]
+        table.setColumnCount(len(headers))
+        table.setHorizontalHeaderLabels(headers)
+        table.setRowCount(len(data) - 1)
+
+        for row_index, row_data in enumerate(data[1:]):
+            for col_index, cell in enumerate(row_data):
+                table.setItem(row_index, col_index, QTableWidgetItem(cell))
 
 class ExpenseTracker(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Expense Tracker")
+        self.setMinimumSize(700,500)
         self.layout = QVBoxLayout()
         icon = qta.icon('fa5s.smile', color='orange')
         
@@ -22,6 +48,7 @@ class ExpenseTracker(QMainWindow):
         data_layout = QFormLayout()
         self.amount_input = QLineEdit()
         self.category = QLineEdit()
+        self.description = QLineEdit()
         self.date =QDateEdit()
         self.date.setDate(QDate.currentDate())
 
@@ -32,14 +59,21 @@ class ExpenseTracker(QMainWindow):
         #Adding rows
         data_layout.addRow("Amount:",self.amount_input)
         data_layout.addRow("Category:",self.category)
+        data_layout.addRow("Description", self.description)
         data_layout.addRow("Date", self.date)
+
+        #Expense table
+        self.table = QTableWidget()
+        loading_csv_table(self.table, data_path)
+        
 
         #Adding layout
         self.layout.addWidget(self.head)
         self.layout.addWidget(self.text_input)
         self.layout.addWidget(data_widget) 
         self.layout.addWidget(self.submit_btn)
-        
+        self.layout.addWidget(self.table)
+
         widget = QWidget()
         widget.setLayout(self.layout)
         self.setCentralWidget(widget)
